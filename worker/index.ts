ce837191e8e922416edc9269697e95e4fdebe321
Soares <js709406@gmail.com>
2025-11-12 01:46:59 -0300

    Ajustar docker para dar start no Temporal e postgress

--- worker/index.ts
+++ worker/index.ts
@@ -1,16 +1,23 @@
-import { Worker } from '@temporalio/worker';
+import { Worker, NativeConnection } from '@temporalio/worker';
 import * as activities from '../src/activities/paymentActivities';
 
 async function run() {
+  const connection = await NativeConnection.connect({
+    address: 'temporal:7233',
+  });
+
   const worker = await Worker.create({
+    connection,
     workflowsPath: require.resolve('../src/workflows/payment.workflow'),
     activities,
     taskQueue: 'payments',
   });
+
+  console.log(' Worker conectado e aguardando tarefas...');
   await worker.run();
 }
 
 run().catch((err) => {
-  console.error(err);
+  console.error(' Erro ao iniciar o worker:', err);
   process.exit(1);
 });
